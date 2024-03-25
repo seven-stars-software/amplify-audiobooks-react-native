7aa690493ab047d07d7e6a954404716170765aba
Jackson Callaway <jcksncllwy@gmail.com>
2024-03-25 16:09:04 -0700

    babel and ts

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ["module-resolver",{
+      root: ['./src'],
+      extensions: [".ios.js", ".android.js", ".js", ".json", ".ts", ".tsx"]
+    }]
+  ]
 };
