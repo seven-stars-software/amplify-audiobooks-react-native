2d348ae2536a76916408149ca20263976299daa8
Jackson Callaway <jcksncllwy@gmail.com>
2025-05-19 18:36:36 -0700

    loading screen quotes

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -6,10 +6,24 @@ import useWelcome, { WelcomeStatus } from "hooks/useWelcome"
 import { RootStackParams } from "navigators/RootNavigator"
 import { useContext, useEffect, useRef, useState } from "react"
 import { ImageBackground, View } from "react-native"
-import { useTheme } from "react-native-paper"
+import { Text, useTheme } from "react-native-paper"
 import { SetupService } from "services"
 
 type Props = NativeStackScreenProps<RootStackParams>
+
+const quips = [
+    "Thanks for AMPlify-ing an author's impact!",
+    "Fetching your audiobooks off the shelf!",
+    "Thanks for using The Equitable Audiobook Platform!",
+    "Every listen helps to AMPlify an author's impact!",
+    "Feeding Starving Authors",
+    "Adopt a starving author today!",
+    "No Authors = No Audiobooks!",
+    "Audiobooks: When reading is just too hard",
+    "Bury Me with an Audiobook",
+    `Building Community
+One audiobook at a time`
+]
 
 const SplashScreen = ({ navigation }: Props) => {
     const theme = useTheme()
@@ -18,6 +32,9 @@ const SplashScreen = ({ navigation }: Props) => {
     const [authSeal,] = useContext(AuthContext);
     const loggedIn = useRef(authSeal !== null);
     loggedIn.current = authSeal !== null
+
+    //Random starting index in the quips array
+    const [quipsIndex, setQuipsIndex] = useState(Math.floor(Math.random() * quips.length));
 
     const { welcomeStatus } = useWelcome();
 
@@ -46,7 +63,7 @@ const SplashScreen = ({ navigation }: Props) => {
             timeout = setTimeout(() => {
                 console.log(`ready check timeout ${ms}: ready? ${ready.current}`)
                 if (ready.current) {
-                    navigateToFirstScreen()
+                    //navigateToFirstScreen()
                 } else {
                     setReadyAfterTimeout(1000)
                 }
@@ -66,7 +83,22 @@ const SplashScreen = ({ navigation }: Props) => {
                 resizeMode='cover'
                 style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
                 source={require('@assets/images/fancy-bg.png')}>
-                <SplashLogo />
+                <View style={{ alignItems: 'center' }}>
+                    <SplashLogo />
+                    <View style={{marginTop: 20, paddingHorizontal: 20}}>
+                        <Text
+                            style={{
+                                color: "white",
+                                textAlign: 'center',
+                                fontSize: 32,
+                                lineHeight: 24,
+                                fontFamily: 'NanumPen-Regular',
+                                paddingVertical: 10
+                            }}>
+                            {quips[quipsIndex]}
+                        </Text>
+                    </View>
+                </View>
             </ImageBackground>
         </View>
     )
