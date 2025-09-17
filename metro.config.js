cb981380af56eb189f1b7aa1dbdac224b9644649
Jackson Callaway <jcksncllwy@gmail.com>
2025-09-17 00:35:59 -0700

    got the app running after lots of upgrades and debugging

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
+const { mergeConfig, getDefaultConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
